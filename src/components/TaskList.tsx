2df4c1413bc23459e86c76ea71fa1310c8af762f
Carlos Heitor <c.heitordiniz@gmail.com>
2021-03-16 23:59:09 -0300

    Implement check task.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -36,6 +36,18 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const selectedTask = tasks.map((task) => {
+
+      if (task.id === id) {
+        task.isComplete = true;
+        return task;
+      } else {
+        return task;
+      }
+    });
+
+    setTasks(selectedTask);
+
   }
 
   function handleRemoveTask(id: number) {
